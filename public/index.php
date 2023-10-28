d9a614e4bb9386103b6cce1ec7562eaa36abf643
鄭哲丞 <a0968518713@gmail.com>
2023-10-29 02:45:23 +0800

    依monolog/monolog在packagist.org的介紹(Basic Usage)，加入monolog測試程式碼至public/index.php

--- public/index.php
+++ public/index.php
@@ -14,3 +14,16 @@ $hello= new Demo\HelloWorld();
 
 use Demo\HelloWorld as World; // 類別另取別名
 $world = new World();
+
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
